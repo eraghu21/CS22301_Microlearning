983d449a55b1e0d29bc39c8636895e0c52791fb8
Raghuvaran Eswaran <66511638+eraghu21@users.noreply.github.com>
2025-10-15 19:59:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,10 +96,7 @@ def create_certificate(name, reg_no, subject):
     pdf.set_font("Helvetica", '', 15)
     pdf.cell(0, 5, f"{reg_no} - II CSE - A", ln=True, align="C")
 
-    pdf.ln(63)
-    pdf.set_font("Helvetica", '', 16)
-    pdf.cell(0, 10, f"Completed on: {timestamp}", ln=True, align="C")
-    
+        
 
     filename = f"{reg_no}_{name.replace(' ', '_')}.pdf"
     path = os.path.join(CERT_DIR, filename)
